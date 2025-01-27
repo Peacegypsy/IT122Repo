54e1f1af7c799933fe06469d4c2403b0b412b2d1
christina <christina.sherman@seattlecolleges.edu>
2025-01-27 14:51:01 -0800

    updating original repo with week 2 homework

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
-import http from "http";
-import fs from "fs";
+'use strict'
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
+import * as album from './data.js'
+import express from "express";
+import {getAlbum} from "./data.js";
 
-    if (url === '/' || url === '/index') {
-        fs.readFile('index.html', (err, data) => {
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('500 - Internal Server Error');
-            }else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        });
-    }else if (url === '/about') {
-        fs.readFile('about.html', (err, data) => {
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('500 - Internal Server Error');
-            }else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        });
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('404 - Internal Server Error');
-    }
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname+ "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home", {albums: album.getAllAlbums()});
 });
-server.listen(3000, () => {
-    console.log('Server started on port 3000');
+
+app.get("/detail", (req, res) => {
+    let album = getAlbum(req.query.artist);
+    res.render("details", {
+        artist: req.query.artist,
+        album: album,
+    });
 });
+
+app.use((req, res) => {
+    res.type("text/plain");
+    res.status(404);
+    res.send("404 - Not Found");
+});
+
+
+app.listen(app.get("port"), () => {
+    console.log("Express started");
+});
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//
+//     if (url === '/' || url === '/index') {
+//         fs.readFile('index.html', (err, data) => {
+//             if (err) {
+//                 res.writeHead(500, {'Content-Type': 'text/plain'});
+//                 res.end('500 - Internal Server Error');
+//             }else {
+//                 res.writeHead(200, {'Content-Type': 'text/html'});
+//                 res.end(data);
+//             }
+//         });
+//     }else if (url === '/about') {
+//         fs.readFile('about.html', (err, data) => {
+//             if (err) {
+//                 res.writeHead(500, {'Content-Type': 'text/plain'});
+//                 res.end('500 - Internal Server Error');
+//             }else {
+//                 res.writeHead(200, {'Content-Type': 'text/html'});
+//                 res.end(data);
+//             }
+//         });
+//     } else {
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         res.end('404 - Internal Server Error');
+//     }
+// });
+// server.listen(3000, () => {
+//     console.log('Server started on port 3000');
+// });
