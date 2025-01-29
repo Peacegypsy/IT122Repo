e6f32d20c3e671be8fd0e79d3829f913b2e575df
christina <christina.sherman@seattlecolleges.edu>
2025-01-28 21:19:17 -0800

    pushing to original repo per instructor request

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
 'use strict'
 
-import * as album from './data.js'
+import {Album} from './models/album.js';
 import express from "express";
-import {getAlbum} from "./data.js";
-
-
+import cors from 'cors';
+ 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
-app.use(express.static(__dirname+ "/public"));
+app.use(express.static( "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("home", {albums: album.getAllAlbums()});
+    Album.find({}).lean()
+        .then((albums) => {
+            res.render('home', {albums: albums});
+        })
+        .catch((err) => {
+            console.error(err);
+        })
 });
+app.get('/home', (req, res) => {
+    Album.find({}).lean()
+        .then((albums) => {
+            res.render('home', {albums: albums});
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
 app.get("/detail", (req, res) => {
-    let album = getAlbum(req.query.artist);
-    res.render("details", {
-        artist: req.query.artist,
-        album: album,
-    });
+    Album.findOne({ artist:req.query.artist}).lean()
+        .then((album) => {
+            res.render('details', {album: album});
+        })
+        .catch((err) => {next(err)})
+});
+app.get('/delete', async (req, res) => {
+    const deletedAlbum = await Album.findOneAndDelete({albumTitle:req.query.albumTitle}).lean();
+
+    if (deletedAlbum) {
+        console.log("Album deleted");
+        res.render('delete', {albumTitle:req.query.albumTitle, deletedAlbum});
+    } else {
+        res.render('delete');
+    }
 });
 
+
 app.use((req, res) => {
-    res.type("text/plain");
-    res.status(404);
-    res.send("404 - Not Found");
+    res.type('text/plain');
+    res.status(404).send('Not Found');
 });
 
 
